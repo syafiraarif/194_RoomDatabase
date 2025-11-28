350c219be849a9dfa84ee154205db5a74d09754f
syairaarif <putrisyafira4444@gmail.com>
2025-11-28 10:44:08 +0700

    meanambahkan class homeviewmodel

---
+++ app/src/main/java/com/example/a194_roomdatabase/viewmodel/HomeViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.a194_roomdatabase.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.example.a194_roomdatabase.repositori.RepositoriSiswa
+
+class HomeViewModel (private val repositoriSiswa: RepositoriSiswa): ViewModel()
+{
+
+}
