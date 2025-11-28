40f3a7185e638444cd82224a637a8592a4ce2f61
syairaarif <putrisyafira4444@gmail.com>
2025-11-28 09:56:24 +0700

    membuat database bernama DatabaseSiswa yang memiliki member fungsi Dao

---
+++ app/src/main/java/com/example/a194_roomdatabase/room/DatabaseSiswa.kt
@@ -0,0 +1,14 @@
+package com.example.a194_roomdatabase.room
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Siswa::class], version = 1, exportSchema = false)
+abstract class DatabaseSiswa : RoomDatabase(){
+
+    abstract fun SiswaDao() : SiswaDao
+
+
+}
