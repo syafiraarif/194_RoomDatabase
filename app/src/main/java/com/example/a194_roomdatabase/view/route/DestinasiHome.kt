f31ae40ea77efc7b490141e96908ed21c5b0cca7
syairaarif <putrisyafira4444@gmail.com>
2025-11-28 12:32:06 +0700

    Ini digunakan untuk menentukan route ke halaman home, dan menentukan judul

---
+++ app/src/main/java/com/example/a194_roomdatabase/view/route/DestinasiHome.kt
@@ -0,0 +1,8 @@
+package com.example.a194_roomdatabase.view.route
+
+import com.example.a194_roomdatabase.R
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
