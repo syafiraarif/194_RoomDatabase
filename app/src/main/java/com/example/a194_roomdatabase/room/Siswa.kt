bf21158c37fde8f9a123e92d37bd390706ecef13
syairaarif <putrisyafira4444@gmail.com>
2025-11-28 09:39:10 +0700

    membuat properti yang merupakan atribut dari class tersebut, berisi 4 atribut

--- app/src/main/java/com/example/a194_roomdatabase/room/Siswa.kt
+++ app/src/main/java/com/example/a194_roomdatabase/room/Siswa.kt
@@ -7,4 +7,9 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "tblSiswa")
 data class Siswa(
+    @PrimaryKey(autoGenerate = true)
+    val id : Int = 0,
+    val nama : String,
+    val alamat : String,
+    val telpon : String
 )
