f3bb51417051cf9ca455d425baf9d48528ec9ce0
syairaarif <putrisyafira4444@gmail.com>
2025-11-28 12:52:45 +0700

    membuat navigasi agar halaman bisa berpindah pindah

--- app/src/main/java/com/example/a194_roomdatabase/view/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/a194_roomdatabase/view/uicontroller/PetaNavigasi.kt
@@ -1,2 +1,48 @@
 package com.example.a194_roomdatabase.view.uicontroller
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.a194_roomdatabase.view.EntrySiswaScreen
+import com.example.a194_roomdatabase.view.HomeScreen
+import com.example.a194_roomdatabase.view.route.DestinasiHome
+import com.example.a194_roomdatabase.view.route.DestinasiEntry
+
+@Composable
+fun SiswaApp(
+    navController: NavHostController = rememberNavController(),
+    modifier: Modifier = Modifier
+) {
+    HostNavigasi(navController = navController, modifier = modifier)
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HostNavigasi(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+) {
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = modifier
+    ) {
+        composable(DestinasiHome.route) {
+            HomeScreen(
+                navigateToItemEntry = {
+                    navController.navigate(DestinasiEntry.route)
+                }
+            )
+        }
+
+        composable(DestinasiEntry.route) {
+            EntrySiswaScreen(
+                navigateBack = { navController.popBackStack() }
+            )
+        }
+    }
+}
