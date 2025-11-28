9ed33ba8c3d0df1e0f7c5fc1b181fe883d9746ac
syairaarif <putrisyafira4444@gmail.com>
2025-11-28 12:14:19 +0700

    membuat data class  dan fungsi yang di butuhkan/menginisialisasi yang di pake di class entryviewmodel

--- app/src/main/java/com/example/a194_roomdatabase/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/a194_roomdatabase/viewmodel/EntryViewModel.kt
@@ -32,5 +32,41 @@ class EntryViewModel( private val repositoriSiswa: RepositoriSiswa) : ViewModel(
     }
 }
 
+data class UIStateSiswa(
+    val detailSiswa: DetailSiswa = DetailSiswa(),
+    val isEntryValid: Boolean = false
+)
+
+data class DetailSiswa(
+    val id: Int = 0,
+    val nama: String = "",
+    val alamat: String = "",
+    val telpon: String = ""
+)
+
+fun DetailSiswa.toSiswa(): Siswa =
+    Siswa(
+        id = id,
+        nama = nama,
+        alamat = alamat,
+        telpon = telpon
+    )
+
+fun Siswa.toDetailSiswa(): DetailSiswa =
+    DetailSiswa(
+        id = id,
+        nama = nama,
+        alamat = alamat,
+        telpon = telpon
+    )
+
+fun Siswa.toUIStateSiswa(isEntryValid: Boolean = false): UIStateSiswa =
+    UIStateSiswa(
+        detailSiswa = this.toDetailSiswa(),
+        isEntryValid = isEntryValid
+    )
+
+
+
 
 
