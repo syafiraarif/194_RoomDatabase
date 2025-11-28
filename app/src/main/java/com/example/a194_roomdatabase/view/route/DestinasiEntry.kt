705b591cc48a418f128419c517627b71a60545fc
syairaarif <putrisyafira4444@gmail.com>
2025-11-28 12:35:12 +0700

    Ini adalah tujuan halaman

---
+++ app/src/main/java/com/example/a194_roomdatabase/view/route/DestinasiEntry.kt
@@ -0,0 +1,8 @@
+package com.example.a194_roomdatabase.view.route
+
+import com.example.a194_roomdatabase.R
+
+object DestinasiEntry: DestinasiNavigasi{
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
