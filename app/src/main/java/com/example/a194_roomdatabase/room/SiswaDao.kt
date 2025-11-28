6e90496a1a35cf60789012ace30fc840c3d4936e
syairaarif <putrisyafira4444@gmail.com>
2025-11-28 09:44:36 +0700

    membuat member dari SiswaDao berupa fungsi bernama insert

--- app/src/main/java/com/example/a194_roomdatabase/room/SiswaDao.kt
+++ app/src/main/java/com/example/a194_roomdatabase/room/SiswaDao.kt
@@ -11,4 +11,7 @@ interface SiswaDao {
     @Query("SELECT * from tblSiswa ORDER BY nama ASC")
     fun getAllSiswa(): Flow<List<Siswa>>
 
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(siswa: Siswa)
+
 }
