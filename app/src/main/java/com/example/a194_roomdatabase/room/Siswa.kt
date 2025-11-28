76617e62f95585b8ac43c046b7501eab0ca2d6ff
syairaarif <putrisyafira4444@gmail.com>
2025-11-28 09:38:11 +0700

    membuat data class bernama Siswa yang di inisialisasi sebagai tblSiswa

---
+++ app/src/main/java/com/example/a194_roomdatabase/room/Siswa.kt
@@ -0,0 +1,10 @@
+package com.example.a194_roomdatabase.room
+
+import android.R
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity(tableName = "tblSiswa")
+data class Siswa(
+)
