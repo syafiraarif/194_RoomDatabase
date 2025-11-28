3323c1af2c77acabee861a19762acbcaed27f7f5
syairaarif <putrisyafira4444@gmail.com>
2025-11-28 13:03:21 +0700

    menambahkan nama aplikasi dan main activity

--- app/src/main/java/com/example/a194_roomdatabase/MainActivity.kt
+++ app/src/main/java/com/example/a194_roomdatabase/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.a194_roomdatabase.ui.theme._194_RoomDatabaseTheme
+import com.example.a194_roomdatabase.view.uicontroller.SiswaApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,8 +21,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             _194_RoomDatabaseTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    SiswaApp(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
