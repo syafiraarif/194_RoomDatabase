6d16afc6c842f86c9cca50d465867b1855244e4d
syairaarif <putrisyafira4444@gmail.com>
2025-11-28 09:44:06 +0700

    membuat member dari SiswaDao berupa fungsi bernama getallsiswa

---
+++ app/src/main/java/com/example/a194_roomdatabase/room/SiswaDao.kt
@@ -0,0 +1,14 @@
+package com.example.a194_roomdatabase.room
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface SiswaDao {
+    @Query("SELECT * from tblSiswa ORDER BY nama ASC")
+    fun getAllSiswa(): Flow<List<Siswa>>
+
+}
